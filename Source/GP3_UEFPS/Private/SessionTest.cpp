1438b1cc3b7ba246fff4c14fbd99b921377ea775
sakata0803 <ktc25a31a0028@edu.kyoto-tech.ac.jp>
2025-10-09 16:24:30 +0900

    2025/10/09の作業分をコミット

---
+++ Source/GP3_UEFPS/Private/SessionTest.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SessionTest.h"
+#include "SessionSubsystem.h"
+
+// Sets default values
+ASessionTest::ASessionTest()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASessionTest::BeginPlay()
+{
+	Super::BeginPlay();
+	if (auto* Sub = GetGameInstance()->GetSubsystem<USessionSubsystem>())
+	{
+		Sub->CreateLanSession(
+			/*PublicConnections=*/3);
+	}
+}
+
+// Called every frame
+void ASessionTest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
--- Source/GP3_UEFPS/Public/SessionSubsystem.h
+++ Source/GP3_UEFPS/Public/SessionSubsystem.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Subsystems/GameInstanceSubsystem.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "Online/OnlineSessionNames.h"
+#include "OnlineSessionSettings.h"
 #include "SessionSubsystem.generated.h"
 
 /**
@@ -13,5 +16,23 @@ UCLASS()
 class GP3_UEFPS_API USessionSubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
-	
+public:
+	UFUNCTION(BlueprintCallable, Category = "Session")
+	void CreateLanSession(int32 PublicConnections = 3);
+private:
+    // Online Subsystem 取得
+    bool EnsureOnline();
+    // デリゲートハンドル管理（解除忘れ防止）
+    void ClearDelegates();
+    // Create の前に既存セッションがあれば壊す
+    void DestroyThenRecreate(int32 PublicConnections);
+    // Online インタフェース
+    IOnlineSubsystem* OSS = nullptr;
+    IOnlineSessionPtr  Session;
+    // デリゲート
+    FDelegateHandle OnCreateHandle;
+    FDelegateHandle OnDestroyHandle;
+    // コールバック
+    void OnCreateComplete(FName SessionName, bool bOk);
+
 };
